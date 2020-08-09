b787a0c8777d24c846ca0d435b44eed3e6d9673e
ankit mishra <am91976@gmail.com>
2020-08-10 01:22:47 +0530

    fixed bug in post detail

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+admin.site.site_header = "Ankit's Career Blog Admin"
+admin.site.site_title = "Ankit's Career Blog Portal"
+admin.site.index_title = "Welcome to Ankit's Career Blog"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
