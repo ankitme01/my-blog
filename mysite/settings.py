4abe61900e6762827447daa08aa63a2a980c699b
ankit mishra <am91976@gmail.com>
2020-08-10 04:49:57 +0530

    added comment,login/logoutand list of post draft feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,6 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 admin.site.site_header = "Ankit's Career Blog Admin"
 admin.site.site_title = "Ankit's Career Blog Portal"
 admin.site.index_title = "Welcome to Ankit's Career Blog"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
